414d4539f692b3c6e770d34fc095ea4ab74b7f3f
ssw03270 <ttd8591@gmail.com>
2020-11-21 21:48:43 +0900

    [Add] - First ~ Sixth UI were finished 일곱 번째 ui는 결과값을 출력할 것이며 이제 inputData를 넘기기만 하면 된다.

--- codes/gui.py
+++ codes/gui.py
@@ -1,12 +1,13 @@
 #encoding='utf-8-sig'
 
 import csv
+import datetime
 import sys
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (QApplication, QWidget, QGroupBox, QHBoxLayout, QRadioButton, QCheckBox, QPushButton, QMenu,
                              QGridLayout, QVBoxLayout, QLabel, QCalendarWidget, QLineEdit, QLayout, QComboBox,
                              QMainWindow)
-from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QDate, Qt
 
 inputData = []
 
@@ -15,6 +16,11 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent)
         self.uiFirst = UIFirst()
         self.uiSecond = UISecond()
+        self.uiThird = UIThird()
+        self.uiFourth = UIFourth()
+        self.uiFifth = UIFifth()
+        self.uiSixth = UISixth()
+        self.uiSeventh = UISeventh()
         self.startUIFirst()
 
     def startUIFirst(self):
@@ -24,12 +30,41 @@ class MainWindow(QMainWindow):
 
     def startUISecond(self):
         self.uiSecond.setupUI(self)
+        self.uiSecond.pushButton1.clicked.connect(self.startUIThird)
+        self.uiSecond.pushButton2.clicked.connect(self.startUIThird)
+        self.show()
+
+    def startUIThird(self):
+        self.uiThird.setupUI(self)
+        self.uiThird.pushButton.clicked.connect(self.startUIFourth)
+        self.show()
+
+    def startUIFourth(self):
+        self.uiFourth.setupUI(self)
+        if(inputData[1] == '왕복'):
+            self.uiFourth.pushButton.clicked.connect(self.startUIFifth)
+        elif(inputData([1] == '편도')):
+            self.uiFourth.pushButton.clicked.connect(self.startUISixth)
+        self.show()
+
+    def startUIFifth(self):
+        self.uiFifth.setupUI(self)
+        self.uiFifth.pushButton.clicked.connect(self.startUISixth)
+        self.show()
+
+    def startUISixth(self):
+        self.uiSixth.setupUI(self)
+        self.uiSixth.pushButton.clicked.connect(self.startUISeventh)
+        self.show()
+
+    def startUISeventh(self):
+        self.uiSeventh.setupUI(self)
         self.show()
 
 class UIFirst(object):
     def setupUI(self, MainWindow):
         MainWindow.setWindowTitle('항공권 비교 분석기')
-        MainWindow.setGeometry(300, 300, 480, 320)
+        MainWindow.setGeometry(300, 300, 720, 480)
 
         self.centralwidget = QWidget(MainWindow)
 
@@ -62,7 +97,6 @@ class UIFirst(object):
         return hbox
 
     def aboutPushButton(self):
-        print(self.comboBox.currentText()[-3:])
         inputData.append(self.comboBox.currentText()[-3:])
 
     def csvRead(self, path):
@@ -73,7 +107,7 @@ class UIFirst(object):
 class UISecond(object):
     def setupUI(self, MainWindow):
         MainWindow.setWindowTitle('항공권 비교 분석기')
-        MainWindow.setGeometry(300, 300, 480, 320)
+        MainWindow.setGeometry(300, 300, 720, 480)
 
         self.centralwidget = QWidget(MainWindow)
 
@@ -101,12 +135,260 @@ class UISecond(object):
         return hbox
 
     def aboutPushButton1(self):
-        print("편도")
         inputData.append("편도")
 
     def aboutPushButton2(self):
-        print("왕복")
         inputData.append("왕복")
+
+class UIThird(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setWindowTitle('항공권 비교 분석기')
+        MainWindow.setGeometry(300, 300, 720, 480)
+
+        self.centralwidget = QWidget(MainWindow)
+
+        self.systemFont = QFont('Arial', 30)
+        self.layoutThird = self.createLayoutThird()  # 세 번째 화면, 최소 출발 기간 입력
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+    def createLayoutThird(self):
+        self.label = QLabel("최소 출발 기한")
+        self.label.setFont(self.systemFont)
+        self.label.setFixedHeight(50)
+
+        self.comboBox1 = QComboBox()
+        self.comboBox1.setFixedHeight(30)
+        self.comboBox1.setFixedWidth(100)
+        self.comboBox1.addItem("년")
+        for year in range(2020, 2022):
+            self.comboBox1.addItem(str(year))
+
+        self.comboBox2 = QComboBox()
+        self.comboBox2.setFixedHeight(30)
+        self.comboBox2.setFixedWidth(100)
+        self.comboBox2.addItem("월")
+        for month in range(1, 13):
+            self.comboBox2.addItem(str(month))
+
+        self.comboBox3 = QComboBox()
+        self.comboBox3.setFixedHeight(30)
+        self.comboBox3.setFixedWidth(100)
+        self.comboBox3.addItem("일")
+        for day in range(1, 32):
+            self.comboBox3.addItem(str(day))
+
+        self.pushButton = QPushButton("확인")
+        self.pushButton.setFont(self.systemFont)
+        self.pushButton.setFixedHeight(50)
+        self.pushButton.setFixedWidth(100)
+        self.pushButton.clicked.connect(self.aboutPushButton)
+
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.label, alignment=Qt.AlignHCenter)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.comboBox1)
+        hbox2.addWidget(self.comboBox2)
+        hbox2.addWidget(self.comboBox3)
+
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.pushButton)
+
+        vbox = QVBoxLayout(self.centralwidget)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        return vbox
+
+    def aboutPushButton(self):
+        inputData.append(self.comboBox1.currentText())
+        inputData.append(self.comboBox2.currentText())
+        inputData.append(self.comboBox3.currentText())
+
+class UIFourth(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setWindowTitle('항공권 비교 분석기')
+        MainWindow.setGeometry(300, 300, 720, 480)
+
+        self.centralwidget = QWidget(MainWindow)
+
+        self.systemFont = QFont('Arial', 30)
+        self.layoutFourth = self.createLayoutFourth()  # 네 번째 화면, 최대 출발 기간 입력
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+    def createLayoutFourth(self):
+        self.label = QLabel("최대 출발 기한")
+        self.label.setFont(self.systemFont)
+        self.label.setFixedHeight(50)
+
+        self.comboBox1 = QComboBox()
+        self.comboBox1.setFixedHeight(30)
+        self.comboBox1.setFixedWidth(100)
+        self.comboBox1.addItem("년")
+        for year in range(2020, 2022):
+            self.comboBox1.addItem(str(year))
+
+        self.comboBox2 = QComboBox()
+        self.comboBox2.setFixedHeight(30)
+        self.comboBox2.setFixedWidth(100)
+        self.comboBox2.addItem("월")
+        for month in range(1, 13):
+            self.comboBox2.addItem(str(month))
+
+        self.comboBox3 = QComboBox()
+        self.comboBox3.setFixedHeight(30)
+        self.comboBox3.setFixedWidth(100)
+        self.comboBox3.addItem("일")
+        for day in range(1, 32):
+            self.comboBox3.addItem(str(day))
+
+        self.pushButton = QPushButton("확인")
+        self.pushButton.setFont(self.systemFont)
+        self.pushButton.setFixedHeight(50)
+        self.pushButton.setFixedWidth(100)
+        self.pushButton.clicked.connect(self.aboutPushButton)
+
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.label, alignment=Qt.AlignHCenter)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.comboBox1)
+        hbox2.addWidget(self.comboBox2)
+        hbox2.addWidget(self.comboBox3)
+
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.pushButton)
+
+        vbox = QVBoxLayout(self.centralwidget)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        return vbox
+
+    def aboutPushButton(self):
+        inputData.append(self.comboBox1.currentText())
+        inputData.append(self.comboBox2.currentText())
+        inputData.append(self.comboBox3.currentText())
+
+class UIFifth(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setWindowTitle('항공권 비교 분석기')
+        MainWindow.setGeometry(300, 300, 720, 480)
+
+        self.centralwidget = QWidget(MainWindow)
+
+        self.systemFont = QFont('Arial', 30)
+        self.layoutFourth = self.createLayoutFifth()  # 다섯 번째 화면, 왕복일 경우 여행 기간 입력
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+    def createLayoutFifth(self):
+        self.lineEdit = QLineEdit("여행 기간 입력 (숫자만, 단위 : 일)")
+        self.lineEdit.setFont(self.systemFont)
+
+        self.pushButton = QPushButton("확인")
+        self.pushButton.setFont(self.systemFont)
+        self.pushButton.clicked.connect(self.aboutPushButton)
+
+        hbox = QHBoxLayout(self.centralwidget)
+        hbox.addWidget(self.lineEdit)
+        hbox.addWidget(self.pushButton)
+        return hbox
+
+    def aboutPushButton(self):
+        inputData.append(self.lineEdit.text())
+
+class UISixth(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setWindowTitle('항공권 비교 분석기')
+        MainWindow.setGeometry(300, 300, 720, 480)
+
+        self.centralwidget = QWidget(MainWindow)
+
+        self.systemFont = QFont('Arial', 30)
+        self.layoutSixth = self.createLayoutSixth()  # 여섯 번째 화면, 탑승 인원 입력
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+    def createLayoutSixth(self):
+        self.label = QLabel("탑승 인원")
+        self.label.setFont(self.systemFont)
+        self.label.setFixedHeight(50)
+
+        self.comboBox1 = QComboBox()
+        self.comboBox1.setFont(self.systemFont)
+        self.comboBox1.setFixedHeight(50)
+        self.comboBox1.setFixedWidth(100)
+        self.comboBox1.addItem("유아")
+        for i in range(0, 11):
+            self.comboBox1.addItem(str(i) + "명")
+
+        self.comboBox2 = QComboBox()
+        self.comboBox2.setFont(self.systemFont)
+        self.comboBox2.addItem("청소년")
+        self.comboBox2.setFixedHeight(50)
+        self.comboBox2.setFixedWidth(100)
+        for i in range(0, 11):
+            self.comboBox2.addItem(str(i) + "명")
+
+        self.comboBox3 = QComboBox()
+        self.comboBox3.setFont(self.systemFont)
+        self.comboBox3.addItem("성인")
+        self.comboBox3.setFixedHeight(50)
+        self.comboBox3.setFixedWidth(100)
+        for i in range(0, 11):
+            self.comboBox3.addItem(str(i) + "명")
+
+        self.pushButton = QPushButton("확인")
+        self.pushButton.setFont(self.systemFont)
+        self.pushButton.setFixedHeight(50)
+        self.pushButton.setFixedWidth(100)
+        self.pushButton.clicked.connect(self.aboutPushButton)
+
+
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.label, alignment=Qt.AlignHCenter)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.comboBox1)
+        hbox2.addWidget(self.comboBox2)
+        hbox2.addWidget(self.comboBox3)
+
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.pushButton)
+
+        vbox = QVBoxLayout(self.centralwidget)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+
+        return vbox
+
+    def aboutPushButton(self):
+        inputData.append(self.comboBox1.currentText())
+        inputData.append(self.comboBox2.currentText())
+        inputData.append(self.comboBox3.currentText())
+        print(inputData)
+
+class UISeventh(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setWindowTitle('항공권 비교 분석기')
+        MainWindow.setGeometry(300, 300, 720, 480)
+
+        self.centralwidget = QWidget(MainWindow)
+
+        self.systemFont = QFont('Arial', 30)
+        self.layoutSeventh = self.createLayoutSeventh()  # 일곱 번째 화면, 결과 출력
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+    def createLayoutSeventh(self):
+
+        vbox = QVBoxLayout(self.centralwidget)
+
+        return vbox
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
