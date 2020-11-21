f4063d364ea7574bede40724c6aeff4d35cf8632
ssw03270 <ttd8591@gmail.com>
2020-11-21 15:18:56 +0900

    [Fix] - UISecond almost finish 두 번째 ui가 대충 마무리 되었다. 세 번째 ui를 제작하고 연결지어주면 될 듯 하다.

--- codes/gui.py
+++ codes/gui.py
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QGroupBox, QHBoxLayout, QRad
                              QMainWindow)
 from PyQt5.QtCore import QDate
 
+inputData = []
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -61,6 +63,7 @@ class UIFirst(object):
 
     def aboutPushButton(self):
         print(self.comboBox.currentText()[-3:])
+        inputData.append(self.comboBox.currentText()[-3:])
 
     def csvRead(self, path):
         fR = open(path, 'r', encoding='utf-8-sig', newline='\n')
@@ -75,38 +78,35 @@ class UISecond(object):
         self.centralwidget = QWidget(MainWindow)
 
         self.systemFont = QFont('Arial', 30)
-        self.layoutFirst = self.createLayoutSecond()  # 첫 번째 화면, 목적지 입력
+        self.layoutSecond = self.createLayoutSecond()  # 두 번째 화면, 편도 or 왕복 선택
 
         MainWindow.setCentralWidget(self.centralwidget)
 
     def createLayoutSecond(self):
-        self.comboBox = QComboBox()
-        self.comboBox.setFixedHeight(50)
-        self.comboBox.setFont(self.systemFont)
-        airportList = []
-        for line in self.csvRead('../airportList.csv'):
-            airportList.append(line[1] + '(' + line[0] + ')' + line[2])
-        for airport in airportList:
-            self.comboBox.addItem(airport)
+        self.pushButton1 = QPushButton("편도")
+        self.pushButton1.setFixedHeight(50)
+        self.pushButton1.setFixedWidth(100)
+        self.pushButton1.setFont(self.systemFont)
+        self.pushButton1.clicked.connect(self.aboutPushButton1)
 
-        self.pushButton = QPushButton("확인")
-        self.pushButton.setFixedHeight(50)
-        self.pushButton.setFixedWidth(100)
-        self.pushButton.setFont(self.systemFont)
-        self.pushButton.clicked.connect(self.aboutPushButton)
+        self.pushButton2 = QPushButton("왕복")
+        self.pushButton2.setFixedHeight(50)
+        self.pushButton2.setFixedWidth(100)
+        self.pushButton2.setFont(self.systemFont)
+        self.pushButton2.clicked.connect(self.aboutPushButton2)
 
         hbox = QHBoxLayout(self.centralwidget)
-        hbox.addWidget(self.pushButton)
-
+        hbox.addWidget(self.pushButton1)
+        hbox.addWidget(self.pushButton2)
         return hbox
 
-    def aboutPushButton(self):
-        print(self.comboBox.currentText()[-3:])
+    def aboutPushButton1(self):
+        print("편도")
+        inputData.append("편도")
 
-    def csvRead(self, path):
-        fR = open(path, 'r', encoding='utf-8-sig', newline='\n')
-        rdr = csv.reader(fR)
-        return rdr
+    def aboutPushButton2(self):
+        print("왕복")
+        inputData.append("왕복")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
